95b8a802ca3936229e544f1090261a9350a9c2ca
Jim Tao <jiwutao@hotmail.com>
2016-11-21 10:43:38 -0500

    Implemented feature1

--- HelloWorldClass.java
+++ HelloWorldClass.java
@@ -3,6 +3,10 @@ public class HelloWorldClass {
 	public static void main(String[] input) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World again!");
+		
+		// Invoke feature 1
+		HelloWorldFeature1 feature1 = new HelloWorldFeature1();
+		feature1.run();
 	}
 
 }
---
+++ HelloWorldFeature1.java
@@ -0,0 +1,6 @@
+
+public class HelloWorldFeature1 {
+	void run() {
+		System.out.println("Feature 1 is implemented.");
+	}
+}
